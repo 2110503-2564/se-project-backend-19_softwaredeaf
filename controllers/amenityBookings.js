e5723f8a0382ee922794cc136b10fee76f5bfd16
Hattakorn <hattakorn8895@gmail.com>
2025-04-19 15:08:00 +0700

    CRUD AmenityBooking

--- controllers/amenityBookings.js
+++ controllers/amenityBookings.js
@@ -1,7 +1,8 @@
 const AmenityBooking = require('../models/AmenityBooking');
 const Camp = require('../models/Camp');
 const CampgroundAmenity = require('../models/CampgroundAmenity');
-const Booking = require('../models/Booking')
+const Booking = require('../models/Booking');
+const { updateAmenity } = require('./amenity');
 
 // @desc    Get all amenity bookings
 // @route   GET /api/v1/amenitybookings
@@ -52,7 +53,7 @@ exports.getAmenityBooking = async (req, res, next) => {
 // @access  Private
 exports.addAmenityBooking = async (req, res, next) => {
     try {
-      const { campgroundAmenityId, amount, startDate, endDate } = req.body;
+      const campgroundAmenityId = req.params.amenityId;
       const campId = req.params.campId;
   
       // 1. Check if camp exists
@@ -67,7 +68,7 @@ exports.addAmenityBooking = async (req, res, next) => {
       // 2. Check if the campgroundAmenityId belongs to the camp
       const amenity = await CampgroundAmenity.findOne({
         _id: campgroundAmenityId,
-        campId: campId // 👈 ชื่อ field ที่ใช้อ้างอิงแคมป์ใน CampgroundAmenity
+        campgroundId: campId // 👈 ชื่อ field ที่ใช้อ้างอิงแคมป์ใน CampgroundAmenity
       });
   
       if (!amenity) {
@@ -76,12 +77,12 @@ exports.addAmenityBooking = async (req, res, next) => {
   
       // 3. Create Amenity Booking
       const amenityBooking = await AmenityBooking.create({
-        campgroundAmenityId,
-        campgroundBookingId: req.body.campgroundBookingId, // ควรตรวจสอบว่า bookingId ถูกต้องด้วย ถ้ามี logic เพิ่ม
-        userId: req.user.id,
-        amount,
-        startDate,
-        endDate
+        userId: "6615dbec06687492895ebfd0", // หรือ req.user._id ถ้าใช้ _id
+        campgroundBookingId: "6615dbec06687492895ebfd0", // จาก URL /:bookingId/amenitybooking
+        campgroundAmenityId: "68031cf782e7300ae6e6bbe7",
+        amount: 2,
+        startDate: "2025-04-25T12:00:00.000Z",
+        endDate: "2025-04-26T12:00:00.000Z"
       });
   
       res.status(201).json({
@@ -93,4 +94,82 @@ exports.addAmenityBooking = async (req, res, next) => {
       console.error(err);
       res.status(500).json({ success: false, message: 'Server Error' });
     }
+  };
+  
+  //@desc     Update amenity booking
+  //@route    PUT /api/v1/amenitybookings/:id
+  //@access   Private
+  exports.updateAmenityBooking = async (req, res, next) => {
+    try {
+            let amenitybooking = await AmenityBooking.findById(req.params.id);
+    
+            if(!amenitybooking) {
+                return res.status(404).json({
+                    success: false,
+                    message: `No booking with the id of ${req.params.id}`
+                });
+            }
+    
+            //Make sure user is the booking owner
+            /*
+            if(amenitybooking.user.toString() !== req.user.id && req.user.role !== 'admin') {
+                return res.status(401).json({
+                    success: false,
+                    message: `User ${req.user.id} is not authorized to update this booking`
+                });
+            }
+            */
+    
+            amenitybooking = await AmenityBooking.findByIdAndUpdate(req.params.id,{
+                userId: "666666666666666666666666", // หรือ req.user._id ถ้าใช้ _id
+                campgroundBookingId: "6615dbec06687492895ebfd0", // จาก URL /:bookingId/amenitybooking
+                campgroundAmenityId: "68031cf782e7300ae6e6bbe7",
+                amount: 1,
+                startDate: "2025-06-25T12:00:00.000Z",
+                endDate: "2025-06-26T12:00:00.000Z"
+            },{
+                new: true,
+                runValidators: true
+            });
+    
+            res.status(200).json({
+                success: true,
+                data: amenitybooking
+            });
+        } catch(err) {
+            console.log(err.stack);
+            return res.status(500).json({
+                success: false,
+                message: "Cannot update AmenityBooking"
+            });
+        }
+  };
+
+  //@desc     Delete amenity booking
+  //@route    DELETE /api/v1/amenitybookings/:id
+  //@access   Private
+  exports.deleteAmenityBooking = async (req, res, next) => {
+    try {
+            const amenitybooking = await AmenityBooking.findById(req.params.id);
+    
+            if(!amenitybooking) {
+                return res.status(404).json({
+                    success: false,
+                    message: `No booking with the id of ${req.params.id}`
+                });
+            }
+    
+            await amenitybooking.deleteOne();
+    
+            res.status(200).json({
+                success: true,
+                data: {}
+            });
+        } catch(err) {
+            console.log(err.stack);
+            return res.status(500).json({
+                success: false,
+                message: "Cannot delete AmenityBooking"
+            });
+        }
   };
--- routes/amenityBookings.js
+++ routes/amenityBookings.js
@@ -1,11 +1,11 @@
 const express = require('express');
 
-const { addAmenityBooking, getAmenityBookings,  getAmenityBooking} = require('../controllers/amenityBookings');
+const { addAmenityBooking, getAmenityBookings,  getAmenityBooking, deleteAmenityBooking, updateAmenityBooking} = require('../controllers/amenityBookings');
 
 const router = express.Router({ mergeParams: true });
 
 // ใช้งาน handler ที่เป็น function จริง ๆ
 router.route('/').get(getAmenityBookings).post(addAmenityBooking);
-router.route('/:id').get(getAmenityBooking);
+router.route('/:id').get(getAmenityBooking).put(updateAmenityBooking).delete(deleteAmenityBooking);
 
 module.exports = router;
