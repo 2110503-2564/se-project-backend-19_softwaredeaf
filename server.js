b341b53cb6d720c8a01c25c7df1f53c0ca5b362c
Hattakorn <hattakorn8895@gmail.com>
2025-04-19 21:47:31 +0700

    add cors

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const dotenv = require("dotenv");
 const cookieParser = require("cookie-parser");
 const connectDB = require("./config/db");
+const cors = require('cors')
 
 //Load env vars
 dotenv.config({ path: "./config/config.env" });
@@ -21,6 +22,8 @@ const app = express();
 
 //Body parser
 app.use(express.json());
+
+app.use(cors())
 
 //Cookie parser
 app.use(cookieParser());
