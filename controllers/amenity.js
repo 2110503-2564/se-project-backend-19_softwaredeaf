3404b99fb4ca101243a18563811408ca6492eee1
Tananan Narkchuay <zackalocloud@gmail.com>
2025-04-19 11:06:11 +0700

    get amenity

--- controllers/amenity.js
+++ controllers/amenity.js
@@ -1,13 +1,14 @@
 const Amenity = require("../models/CampgroundAmenity");
 
 exports.getAmenity = async (req, res, next) => {
-  const amenity = await Amenity.findO; // filter by campgroundI
+  const amenity = await Amenity.findOne({ campgroundId: req.params.campId });
   if (!amenity) {
     return res.status(404).json({
       success: false,
       message: "Amenity not found for this campground",
     });
   }
+
   res.status(200).json({ success: true, data: amenity });
 };
 
