dbef0569b1fa0521a3ad133b7ebf6e6a8409b528
Hattakorn <hattakorn8895@gmail.com>
2025-04-20 12:52:08 +0700

    edit sendTokenResponse

--- controllers/auth.js
+++ controllers/auth.js
@@ -88,9 +88,11 @@ const sendTokenResponse = (user, statusCode, res) => {
     options.secure = true;
   }
 
-  res.status(statusCode).cookie("token", token, options).json({
-    success: true,
-    token,
+  res.status(statusCode).cookie('token',token,options).json({success:true,_id: user._id,
+    name: user.name,
+    email: user.email,
+    role:user.role,
+    token
   });
 };
 
