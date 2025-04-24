d978e5cba3bb016f9ddb8ff934064e57eeb326c8
Tananan Narkchuay <zackalocloud@gmail.com>
2025-04-25 00:41:01 +0700

    user can review now and has authorization

--- routes/userReviews.js
+++ routes/userReviews.js
@@ -1,11 +1,13 @@
 const express = require('express');
-const { getMyReview } =require('../controllers/review')
+const { getMyReview,createReview } =require('../controllers/review')
 
 const router = express.Router();
 
 
 const {protect,authorize} = require('../middleware/auth');
 
-router.route('/:id').get(getMyReview);
+router.route('/').post(protect,authorize('user','owner','admin'),createReview);
+router.route('/:id').get(protect,authorize('user','owner','admin'),getMyReview)
+
 
 module.exports = router;
