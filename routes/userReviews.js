ba62bc23c320897e6cf3d0b9ef10afdd746f0fb3
Tananan Narkchuay <zackalocloud@gmail.com>
2025-04-25 01:07:48 +0700

    fix delete

--- routes/userReviews.js
+++ routes/userReviews.js
@@ -1,13 +1,20 @@
-const express = require('express');
-const { getMyReview,createReview } =require('../controllers/review')
+const express = require("express");
+const {
+  getMyReview,
+  createReview,
+  deleteReview,
+} = require("../controllers/review");
 
 const router = express.Router();
 
+const { protect, authorize } = require("../middleware/auth");
 
-const {protect,authorize} = require('../middleware/auth');
-
-router.route('/').post(protect,authorize('user','owner','admin'),createReview);
-router.route('/:id').get(protect,authorize('user','owner','admin'),getMyReview)
-
+router
+  .route("/")
+  .post(protect, authorize("user", "owner", "admin"), createReview);
+router
+  .route("/:id")
+  .get(protect, authorize("user", "owner", "admin"), getMyReview)
+  .delete(protect, authorize("user", "owner", "admin",deleteReview));
 
 module.exports = router;
