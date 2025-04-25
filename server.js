325d405fd0486672783d7dd071a941354f873b5f
Woraphat Jittichanon <guid.4012@gmail.com>
2025-04-25 09:13:36 +0700

    Merge with CRUD Image

--- server.js
+++ server.js
@@ -20,8 +20,9 @@ const amenityBookings = require('./routes/amenityBookings');
 
 const app = express();
 
-//Body parser
-app.use(express.json());
+//body parser
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 app.use(cors())
 
