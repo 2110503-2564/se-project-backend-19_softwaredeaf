f3f408cd48f117caa8c9dcc312598767b3ee367c
Tananan Narkchuay <zackalocloud@gmail.com>
2025-04-29 18:59:18 +0700

    cant use reserve word

--- controllers/s3.js
+++ controllers/s3.js
@@ -10,7 +10,7 @@ console.log("Region:", process.env.AWS_BUCKET_REGION);
 
 const bucketName = process.env.AWS_BUCKET_NAME;
 const region = process.env.AWS_BUCKET_REGION;
-const accessKeyId = process.env.AWS_ACCESS_KEY;
+const accessKeyId = process.env.GD_AWS_ACCESS_KEY;
 const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY;
 
 const s3Client = new S3Client({
